557dfd15aaab16ca20dcb0117b5f9b9f38b78108
Matthew John Lapehn <mlapehn@gmail.com>
2016-08-15 19:01:02 -0600

    Weekend HW

--- challenge_2/js/app.js
+++ challenge_2/js/app.js
@@ -1,8 +1,31 @@
+
 console.log('challenge #2 -- app.js loaded!');
+var app = angular.module("appTwo", []);
 
-// var app = angular.module("appTwo", []);
-// app.controller("WelcomeController", WelcomeController);
+app.controller("WelcomeController", WelcomeController);
 
-// function WelcomeController(){
-//   this.full_name = "YOUR NAME"
-// }
+function WelcomeController() {
+	this.full_name = "Jamie Scovern";
+	this.age = 30;
+	this.city = "Bala Cynwyd";
+	this.state = "Pennsylvania";
+	this.shout = function(str) {
+		return str.toUpperCase()+"!";
+	};
+}
+
+app.controller("ClassController", ClassController);
+
+function ClassController() {
+	this.class_name="DEN-WDI-1";
+	this.enrolled_students=14;
+	this.start_date = new Date(2016,5,13);
+	this.end_date = new Date(2016,8,2);
+	this.daysRemaining = function() {
+		var today = new Date();
+		today=today.getTime();
+		var end = this.end_date.getTime();
+		var msInDay = 1000*60*60*24;
+		return Math.round( (end - today)/msInDay );
+	};
+}
--- challenge_3/js/app.js
+++ challenge_3/js/app.js
@@ -1,9 +1,22 @@
 console.log('challenge #3 -- app.js loaded!');
+var app = angular.module("appThree",[]);
+app.controller("infoController", infoController);
 
-// var app = angular.module("appThree", []);
-// app.controller("infoController", infoController)
+function infoController(){
+  this.name = "Jon Doe"; // default name
+  this.height = 5.6; // default height in feet
+}
 
-// function infoController(){
-//   this.name = "Jon Doe"; // default name
-//   this.height = 5.6; // default height in feet
-// }
+app.controller("loanController", loanController);
+
+function loanController() {
+	this.apr=25;
+	this.amount=100000;
+	var monthly_interest = (this.apr/100/12) * this.amount;
+	this.month = function(calcMonth) {
+		return {
+			accrued_interest: Math.round(calcMonth * monthly_interest),
+			total_balance: Math.round(this.amount + calcMonth * monthly_interest)
+		}
+	};
+}
